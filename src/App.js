8975f411b9680b4cbd6d3e4cb3be2f0ce969fcf5
shagors <shajjadshagor@gmail.com>
2022-08-20 09:40:34 +0600

    try basic design and validate form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import FormStyle from './components/Form/FormStyle';
 
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
+      <FormStyle></FormStyle>
     </div>
   );
 }
